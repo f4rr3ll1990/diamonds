d258c93f234817782e67f9dc7172e9ecf2b742a3
F4rr3LL <00mazahaka00@gmail.com>
2021-10-08 22:33:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var elem = document.querySelector('html');
+var elem = document.querySelector('body');
 
 // Set color to purple
 elem.style.opacity = '0.5';
-console.log('success')
+console.log('success!')
 
